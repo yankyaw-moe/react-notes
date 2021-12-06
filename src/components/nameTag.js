535ae2470624ac2287eed996d271811de2399c6b
yankyawmoe <yankyawmoe599@gmail.com>
2021-12-06 17:52:18 +0630

    Conditional Rendering

--- src/components/nameTag.js
+++ src/components/nameTag.js
@@ -2,14 +2,19 @@
 import React from "react";
 
 function nameTag(props) {
-  // <Fragment></Fragment>
-  // <></>
-  // <div></div>
-  // [] => return Array
+  if (!props.firstName || !props.lastName) {
+    return (
+      <div className="name">
+        <h3>Invalid Name</h3>
+      </div>
+    );
+  }
+
   return (
     <div className="name">
       <h3>First Name {props.firstName}</h3>
       <h3>Last Name {props.lastName}</h3>
+      {props.firstName === "Thet" && <div style={{ color: "yellow" }}>VIP</div>}
     </div>
   );
 }
