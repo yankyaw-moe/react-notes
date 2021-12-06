c038d3bfab8e107d72e3d75aeb4bc6e1ab6b1a50
yankyawmoe <yankyawmoe599@gmail.com>
2021-12-06 14:07:05 +0630

    Reusable Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.scss";
 import React from "react";
+import NameTag from "./components/nameTag";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Names List</h1>
-        <h3 className="name">Thet Moe Swe</h3>
-        <h3 className="name">Pan Ei Phyu</h3>
-        <h3 className="name">Zin Moe Ei</h3>
+        <NameTag></NameTag>
+        <NameTag></NameTag>
+        <NameTag></NameTag>
       </header>
     </div>
   );
---
+++ src/components/nameTag.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function nameTag() {
+  return <h3 className="name">Yan</h3>;
+}
+
+export default nameTag;
