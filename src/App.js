06767ef25b6048e7816982938ce5254165b7e8de
yankyawmoe <yankyawmoe599@gmail.com>
2021-12-07 21:47:33 +0630

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,54 +1,26 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
 import "./App.scss";
-import React from "react";
+import React, { useState } from "react";
 import NameTag from "./components/nameTag";
 
-const makeGreen = (BaseComponent) => (props) => {
-  console.log("props >> ", props);
-  const newProps = {
-    style: {
-      color: "green",
-    },
-  };
-
-  const updatedProps = {
-    ...props,
-    ...newProps,
-  };
-
-  return <BaseComponent {...updatedProps} />;
-};
-
-const removeInline = (BaseComponent) => (props) => {
-  const updateProps = {
-    ...props,
-  };
-  delete updateProps.style;
-  return <BaseComponent {...updateProps} />;
-};
-
-const MakeGreen = makeGreen(NameTag);
-const RemoveInline = removeInline(NameTag);
-
 function App() {
+  const [age, setAge] = useState(26);
+
+  const ageUpHandle = () => {
+    setAge(age + 1);
+  };
+
+  const ageDownHandle = () => {
+    setAge(age - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Names List</h1>
-        <MakeGreen firstName="Thet" lastName="Moe"></MakeGreen>
-        <RemoveInline
-          style={{ color: "red" }}
-          firstName="Pan"
-          lastName="Ei"
-        ></RemoveInline>
-        <NameTag
-          style={{ color: "blue" }}
-          firstName="Zin"
-          lastName="Moe"
-        ></NameTag>
-
-        <input type="text" placeholder="Enter Name"></input>
+        <h2>Age {age}</h2>
+        <button onClick={ageUpHandle}>age up</button>
+        <button onClick={ageDownHandle}>age down</button>
       </header>
     </div>
   );
